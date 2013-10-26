8680f27450840ca2f9a2f65a7fd21cc499905f8a
Jonas Krook <krook.jonas+github@gmail.com>
2013-10-26 08:51:33 +0200

    Load EdX login screen in WebView on create.

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
@@ -1,15 +1,25 @@
 package com.github.krooken.edxvideoviewer;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.webkit.WebView;
 
 public class LoginScreen extends Activity {
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_screen);
+		
+		WebView loginWebView = (WebView)findViewById(R.id.login_web_view);
+		loginWebView.getSettings().setJavaScriptEnabled(true);
+		loginWebView.loadUrl("https://courses.edx.org/login");
+		getWindow().setSoftInputMode(
+			      WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
 
 	@Override
