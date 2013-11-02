7ce411b391f2a76203f0f1777139d4f61096eeed
Jonas Krook <krook.jonas+github@gmail.com>
2013-11-02 17:11:11 +0100

    Created thread for getting /courseware for selected course. Prints the header values and names to debug.

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CoursewareContentViewer.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CoursewareContentViewer.java
@@ -3,16 +3,21 @@ package com.github.krooken.edxvideoviewer;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.http.Header;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
 public class CoursewareContentViewer extends Activity {
 
 	private ArrayAdapter<String> adapter;
 	private String cookieData;
+	
+	private static final String TAG = "CoursewareContentViewer";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,10 @@ public class CoursewareContentViewer extends Activity {
 		Intent intent = getIntent();
 		cookieData = intent.getCharSequenceExtra("cookie_data").toString();
 		String courseAddress = intent.getCharSequenceExtra("course_info_address").toString();
+		courseAddress = courseAddress.replace("/info", "/coursware");
 		HttpGetRequest getRequest = null;
 		try {
-			getRequest = new HttpGetRequest(new URI("https://courses.edx.org/dashboard"));
+			getRequest = new HttpGetRequest(new URI("https://courses.edx.org/" + courseAddress));
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,6 +41,23 @@ public class CoursewareContentViewer extends Activity {
 		final HttpGetRequest request = getRequest;
 
 		final Handler responseHandler = new Handler();
+		
+		Thread headerThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				HttpGetRequest getRequest = request;
+				request.executeGetRequest();
+				Header[] responseHeaders = request.getResponseHeaders();
+				for(int i=0; i<responseHeaders.length; i++) {
+					Log.d(TAG, "Header name: " + responseHeaders[i].getName() + 
+							" value: " + responseHeaders[i].getValue());
+				}
+			}
+		});
+		
+		headerThread.start();
 	}
 
 }
