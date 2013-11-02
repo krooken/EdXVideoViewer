a6cb3eb7723e5f82dbb91ae484d5480f843c12f3
Jonas Krook <krook.jonas+github@gmail.com>
2013-11-02 08:28:27 +0100

    Removed unused import.

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CourseViewer.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CourseViewer.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
