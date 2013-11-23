6d2afc57aaf3ad3cd61074393ed8ebe360b2b1ab
Jonas Krook <krook.jonas+github@gmail.com>
2013-11-23 10:35:53 +0100

    Initialized arrays with zero elements instead of 1 element. fixes #7

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CoursewareSectionViewer.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/CoursewareSectionViewer.java
@@ -33,8 +33,8 @@ public class CoursewareSectionViewer extends Activity {
 	
 	private static final String TAG = "CoursewareSectionViewer";
 	
-	private String[] videoTexts = new String[1];
-	private String[] videoAddresses = new String[1];
+	private String[] videoTexts = new String[0];
+	private String[] videoAddresses = new String[0];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
