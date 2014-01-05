a46f74ff19178c09e9654c0f31157f3ad4d7387c
Jonas Krook <krook.jonas+github@gmail.com>
2014-01-05 17:52:34 +0100

    Removed all code related to WebView.
    Removed some debug code.
    
    Created a login button.
    
    #11

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
@@ -11,17 +11,12 @@ import org.apache.http.Header;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager;
-import android.webkit.CookieManager;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -30,23 +25,10 @@ import android.widget.TextView;
 public class LoginScreen extends Activity {
 	
 	private static String TAG = "LoginScreenActivity";
-
-	private boolean userDetailsSubmitted = false;
-	private boolean pageHasLoaded = false;
-	private WebView loginWebView;
-	private String userName;
-	private String password;
-	private String cookieData = "";
-	private boolean remember;
-
-	private boolean dataAdded = false;
-
-	private boolean loggedIn = false;
 	
 	private final Handler handler = new Handler();
 	private boolean loginInProgress = false;
 
-	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -60,117 +42,17 @@ public class LoginScreen extends Activity {
 					new UncaughtExceptionLogger(file.getAbsolutePath(), defaultUEH));
 		}
 		
-		loginWebView = (WebView)findViewById(R.id.login_web_view);
+		Button loginButton = (Button)findViewById(R.id.login_button);
 		
-		CookieManager cookieMgr = CookieManager.getInstance();
-		String cookieMgrStr = "";
-		if(cookieMgr == null) {
-			cookieMgrStr = "null";
-		}else{
-			String rawCookieHeader = cookieMgr.getCookie("courses.edx.org");
-			if(rawCookieHeader == null) {
-				cookieMgrStr = "no cookie data!";
-			}else {
-				cookieMgrStr = rawCookieHeader;
-			}
-		}
-		
-		Log.d(TAG, "CookieMgr before load: " + cookieMgrStr);
-		
-		loginWebView.getSettings().setJavaScriptEnabled(true);
-		loginWebView.loadUrl("https://courses.edx.org/login");
-		getWindow().setSoftInputMode(
-			      WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-		
-		loginWebView.setWebViewClient(new WebViewClient() {
-
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-				
-				Log.d(TAG,"onPageFinished(): Loaded completed!");
-				
-				pageHasLoaded = true;
-				tryAddDataToForm();
-				
-				CookieManager cookieMgr = CookieManager.getInstance();
-				String cookieMgrStr = "";
-				if(cookieMgr == null) {
-					cookieMgrStr = "null";
-				}else{
-					String rawCookieHeader = cookieMgr.getCookie("courses.edx.org");
-					if(rawCookieHeader == null) {
-						cookieMgrStr = "no cookie data!";
-					}else {
-						cookieMgrStr = rawCookieHeader;
-					}
-				}
-				
-				Log.d(TAG, "CookieMgr after load: " + cookieMgrStr);
-				
-				CookieManager mgr = CookieManager.getInstance();
-				String cookies = mgr.getCookie("courses.edx.org");
-				cookieData = cookies;
-				Pattern edxLoggedInPattern = Pattern.compile("edxloggedin=([^;]+);");
-				Pattern sessionIdPattern = Pattern.compile("sessionid=([^;]+);");
-				Matcher edxLoggedInMatcher = edxLoggedInPattern.matcher(cookies);
-				Matcher sessionIdMatcher = sessionIdPattern.matcher(cookies);
-				if(edxLoggedInMatcher.find()) {
-					Log.d(TAG, edxLoggedInMatcher.group(1));
-					if(edxLoggedInMatcher.group(1).equalsIgnoreCase("true")) {
-						loggedIn = true;
-					}
-				}
-				if(sessionIdMatcher.find()) {
-					Log.d(TAG, sessionIdMatcher.group(1));
-				}
-			}
-
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				Log.d(TAG,"ShouldOverride returns false.");
-				
-				return false;
-			}
-			
-		});
-		
-		Button addDataToFormButton = (Button)findViewById(R.id.add_data_button);
-		Button sendDataButton = (Button)findViewById(R.id.send_data_button);
-		Button continueButton = (Button)findViewById(R.id.continue_button);
-		
-		addDataToFormButton.setOnClickListener(new OnClickListener() {
+		loginButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				userName = ((EditText)findViewById(R.id.username_field)).getText().toString();
-				password = ((EditText)findViewById(R.id.password_field)).getText().toString();
-				remember = ((CheckBox)findViewById(R.id.remember_field)).isChecked();
-				
-				userDetailsSubmitted = true;
-				tryAddDataToForm();
-			}
-		});
-		
-		sendDataButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String username = ((EditText)findViewById(R.id.username_field)).getText().toString();
-				String password = ((EditText)findViewById(R.id.password_field)).getText().toString();
-				tryLogin(username, password, remember);
-			}
-		});
-		
-		continueButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				//TODO: Start new activity
-				if(loggedIn) {
-					Intent intent = new Intent(LoginScreen.this, CourseViewer.class);
-					intent.putExtra("cookie_data", cookieData);
-					startActivity(intent);
+				if(!loginInProgress) {
+					String username = ((EditText)findViewById(R.id.username_field)).getText().toString();
+					String password = ((EditText)findViewById(R.id.password_field)).getText().toString();
+					boolean remember = ((CheckBox)findViewById(R.id.remember_field)).isChecked();
+					tryLogin(username, password, remember);
 				}
 			}
 		});
@@ -181,10 +63,6 @@ public class LoginScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login_screen, menu);
 		return true;
-	}
-	
-	private boolean isLoginReady() {
-		return userDetailsSubmitted && pageHasLoaded;
 	}
 	
 	private void tryLogin(String username, String password, boolean remember) {
@@ -211,7 +89,6 @@ public class LoginScreen extends Activity {
 				String csrfToken = null;
 				
 				for(int i=0; i<headers.length; i++) {
-					Log.d(TAG, "Header " + i + ": " + headers[i].getName() + " value: " + headers[i].getValue());
 					
 					if(headers[i].getName().equals("Set-Cookie")) {
 						String cookieString = headers[i].getValue();
@@ -258,7 +135,6 @@ public class LoginScreen extends Activity {
 				String edxloggedincookie = null;
 				
 				for(int i=0; i<headers.length; i++) {
-					Log.d(TAG, "Header " + i + ": " + headers[i].getName() + " value: " + headers[i].getValue());
 					
 					if(headers[i].getName().equals("Set-Cookie")) {
 						String cookieString = headers[i].getValue();
@@ -305,33 +181,6 @@ public class LoginScreen extends Activity {
 		
 		loginInProgress = true;
 		thread.start();
-	}
-	
-	private void tryAddDataToForm() {
-		
-		Log.d(TAG, "tryAddDataToForm(): submitted: " + userDetailsSubmitted + " loaded: " + pageHasLoaded);
-		
-		if(isLoginReady()) {
-			loginWebView.loadUrl("javascript:" + 
-					"(function() {" +
-					"document.getElementById('email').value = '" + userName + "';" +
-					"document.getElementById('password').value = '" + password + "';" +
-					"document.getElementById('remember-yes').checked = " + (remember ? "'true'" : "''") + ";" +
-					"})()");
-			
-			dataAdded  = true;
-		}
-	}
-	
-	private void tryLoginFromPrefilledForm() {
-		if(dataAdded) {
-			loginWebView.loadUrl("javascript:" + 
-					"(function() {" + 
-					"document.getElementById('submit').click();" +
-					"})()");
-			
-			loggedIn  = true;
-		}
 	}
 	
 	private void loginSuccessful(String sessionId, String edxLoggedIn) {
