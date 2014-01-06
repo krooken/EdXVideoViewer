adb4ea21c794aee8dcdc96c26e6fe5b9254d5699
Jonas Krook <krook.jonas+github@gmail.com>
2014-01-06 18:12:31 +0100

    Added session memory. If a valid session exists, the login screen is automatically skipped. #11

--- EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
+++ EdXVideoViewer/src/com/github/krooken/edxvideoviewer/LoginScreen.java
@@ -12,7 +12,9 @@ import org.apache.http.Header;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -21,6 +23,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class LoginScreen extends Activity {
@@ -29,6 +32,7 @@ public class LoginScreen extends Activity {
 	
 	private final Handler handler = new Handler();
 	private boolean loginInProgress = false;
+	private boolean startupInProgress = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,60 @@ public class LoginScreen extends Activity {
 			Thread.setDefaultUncaughtExceptionHandler(
 					new UncaughtExceptionLogger(file.getAbsolutePath(), defaultUEH));
 		}
+		
+		Thread alreadyLoggedInThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+				String sessionId = prefs.getString("session_cookie", null);
+				String edxLoggedIn = prefs.getString("edxloggedin_cookie", null);
+				
+				boolean loggedIn = false;
+				
+				if(sessionId != null && edxLoggedIn != null) {
+					HttpGetRequest httpRequest = null;
+					try {
+						httpRequest = new HttpGetRequest(new URI(
+								"https://courses.edx.org/dashboard"));
+					} catch (URISyntaxException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					httpRequest.addCookieHeader(sessionId);
+					httpRequest.addCookieHeader(edxLoggedIn);
+					
+					httpRequest.executeGetRequest();
+					
+					if(httpRequest.getStatusCode() == 200) {
+						loggedIn = true;
+					} else {
+						loggedIn = false;
+					}
+				} else {
+					loggedIn = false;
+				}
+				
+				if(loggedIn) {
+					handler.post(new Runnable() {
+						public void run() {
+							alreadyLoggedIn();
+						}
+					});
+				} else {
+					handler.post(new Runnable() {
+						
+						@Override
+						public void run() {
+							needToLogIn();
+						}
+					});
+				}
+			}
+		});
+		alreadyLoggedInThread.start();
 		
 		Button loginButton = (Button)findViewById(R.id.login_button);
 		
@@ -188,10 +246,13 @@ public class LoginScreen extends Activity {
 	}
 	
 	private void loginSuccessful(String sessionId, String edxLoggedIn) {
+		SharedPreferences.Editor prefsEditor = getPreferences(Context.MODE_PRIVATE).edit();
+		prefsEditor.putString("session_cookie", sessionId);
+		prefsEditor.putString("edxloggedin_cookie", edxLoggedIn);
+		prefsEditor.commit();
+		
 		String cookieString = sessionId + edxLoggedIn;
-		Intent intent = new Intent(LoginScreen.this, CourseViewer.class);
-		intent.putExtra("cookie_data", cookieString);
-		startActivity(intent);
+		leaveLoginActivity(cookieString);
 		loginProgressStop();
 	}
 	
@@ -206,6 +267,25 @@ public class LoginScreen extends Activity {
 		loginInProgress = false;
 		((Button)findViewById(R.id.login_button)).setEnabled(true);
 		((ProgressBar)findViewById(R.id.login_in_progress_bar)).setVisibility(View.GONE);
+	}
+	
+	private void alreadyLoggedIn() {
+		SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+		String cookies = prefs.getString("session_cookie", "") +
+				prefs.getString("edxloggedin_cookie", "");
+		leaveLoginActivity(cookies);
+	}
+	
+	private void needToLogIn() {
+		((ProgressBar)findViewById(R.id.already_logged_in_progress_bar)).setVisibility(View.GONE);
+		((RelativeLayout)findViewById(R.id.login_form_container)).setVisibility(View.VISIBLE);
+		startupInProgress = false;
+	}
+	
+	private void leaveLoginActivity(String cookies) {
+		Intent intent = new Intent(LoginScreen.this, CourseViewer.class);
+		intent.putExtra("cookie_data", cookies);
+		startActivity(intent);
 	}
 
 }
